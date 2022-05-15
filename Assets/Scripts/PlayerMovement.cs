16153425320a06f60447c02575ecd65f06e7e4f3
Lucas_Miras-ZirTyz <lucas.miras@live.fr>
2022-05-14 20:41:35 -0400

    Add gravity and Tags for wall ground and pole

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private float groundDistance = 0.1f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    [SerializeField]
     private CharacterController controller;
 
     [SerializeField]
     private float speed = 12f;
     // Start is called before the first frame update
+    public float gravity = -9.81f;
+
+    [SerializeField]
+    Vector3 velocity;
+
     void Start()
     {
         
@@ -18,11 +30,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Check on ground & reset gravity velocity
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y <0f) 
+        {
+            velocity.y = -4f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
     
         Vector3 move = transform.right * x + transform.forward * z; 
     
+        //gravity
+        velocity.y += gravity * Time.deltaTime; 
+        controller.Move(velocity * Time.deltaTime);
+
+        //deplacement
         controller.Move(move * speed * Time.deltaTime);
     }
 
