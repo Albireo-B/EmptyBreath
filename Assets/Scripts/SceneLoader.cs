791a5e7df67ccb6934a59beaf011a48807757d81
Nico <nicey74300@gmail.com>
2022-06-19 19:57:23 -0400

    Add menu and credit scene modified

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Jouer()
+    {
+        SceneManager.LoadScene("EmptyBreath");
+
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
